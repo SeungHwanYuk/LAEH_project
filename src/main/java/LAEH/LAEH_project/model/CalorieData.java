d9ea3985c9f4486022e5474f9a9143dd68cb1af8
SeungHwanYuk <ysh54244902@gmail.com>
2024-06-05 11:34:46 +0900

    init

---
+++ src/main/java/LAEH/LAEH_project/model/CalorieData.java
@@ -0,0 +1,19 @@
+package LAEH.LAEH_project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "성분정리표")
+public class CalorieData {
+
+    private String 
+}
