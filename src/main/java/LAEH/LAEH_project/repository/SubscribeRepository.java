bb0bc1f79159f3a3d3437c417ee78271e1b05fcf
SeungHwanYuk <ysh54244902@gmail.com>
2024-06-18 12:43:31 +0900

    124

--- src/main/java/LAEH/LAEH_project/repository/SubscribeRepository.java
+++ src/main/java/LAEH/LAEH_project/repository/SubscribeRepository.java
@@ -3,5 +3,5 @@ package LAEH.LAEH_project.repository;
 import LAEH.LAEH_project.model.Subscribe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SubscribeRepository extends JpaRepository<Subscribe, String > {
+public interface SubscribeRepository extends JpaRepository<Subscribe, Long > {
 }
