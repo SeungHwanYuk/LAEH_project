c6f14466006c77481124a54e4e91a8edfa78891b
star0717 <ydb0717@gmail.com>
2024-06-19 15:48:44 +0900

    a

--- src/main/resources/static/js/yogaClass.js
+++ src/main/resources/static/js/yogaClass.js
@@ -48,3 +48,31 @@ function displayContents(contentsData) {
     });
   }
 }
+
+const urlCurrent = "http://localhost:8080/user/current";
+
+function sessionCurrent() {
+  // 로그인 유지 확인 코드
+  axios
+    .get(urlCurrent, { withCredentials: true })
+    .then((response) => {
+      console.log("데이터", response);
+      if (response.data.userId == "anonymousUser") {
+        alert("로그인이 필요합니다.");
+        window.location.href = "login.html";
+      } else {
+        //   if (response.data.userId != "anonymousUser") {
+        console.log("세션 유지");
+        if (response.status == 200) {
+          console.log(response.data.userId + "님, 환영합니다.");
+          document.querySelector(".logout").classList.remove("hidden");
+          document.querySelector(".login").classList.add("hidden");
+          document.querySelector(".join").classList.add("hidden");
+        }
+      }
+    })
+    .catch((error) => {
+      console.log("에러 발생", error);
+    });
+}
+sessionCurrent();
